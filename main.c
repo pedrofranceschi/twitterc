4232ee0c95a74fe7dcc34a749d14e3e092ddbaa0
Pedro Franceschi <pedrohfranceschi@gmail.com>
2012-10-10 23:36:48 -0300

    Removed unnecessary comments.

--- main.c
+++ main.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
 #include "twitter_api.h"
 
-int main() {
-	// HTTPConnection httpConnection;
-	// HTTPConnection_initialize(&httpConnection);
-	// httpConnection.url = "http://www.google.com.br/";
-	// 
-	// HTTPConnection_perform_request(&httpConnection);
-	// 
-	// // printf(" response buffer: %s\n", httpConnection.response_buffer);
-	// 
-	// HTTPConnection_free(&httpConnection);
-	
+int main() {	
 	// while(1 == 1) {
 	Tweet *first_search_result;
 	int asd = TwitterAPI_search("iphone", &first_search_result);
-	
-	// printf("first_search_result 2: %i\n", first_search_result);
 	
 	Tweet *current_tweet = first_search_result;
 	while(current_tweet != NULL) {
